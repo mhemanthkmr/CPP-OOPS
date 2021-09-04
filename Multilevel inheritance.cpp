b18e6d4bc4b77dff9176d925a96b9fd57fe8ae93
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-09-04 20:38:33 +0530

    Multilevel INheritance

---
+++ Multilevel inheritance.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std ;
+/*
+
+Inheritance
+
+Inheritance is the process by which object of one class 
+acquired the properties of another class .
+
+There are Five Type of Inheritance 
+
+1. Single Inheritance 
+2. Multiple Inheritance
+3. Multolevel Inheritance
+4. Hierarcichal Inheritance
+5. Hybrid Inheritance 
+*/
+
+
+class GrandFather
+{
+public:	
+	void House()
+	{
+		cout<<"Have 3BHK House"<<endl;
+	}
+};
+class Father:public GrandFather
+{
+public:
+	void Land()
+	{
+		cout<<"2 Acrs Land"<<endl;
+	}
+};
+class Son:public Father
+{
+public:
+	void Car()
+	{
+		cout<<"Have Audi A6 Car"<<endl;
+	}
+};
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif	
+
+	Son o;
+	o.Car();  
+	o.Land(); 
+	o.House();
+
+	return 0;
+}
